4b3d30ed8f70dc7c50d369c0e06206762e4ce429
Naveen Anjuru <Naveen.Anjuru@synechron.com>
2017-01-24 17:30:46 +0530

    anges

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
     
     @RequestMapping("/")
-    public String hello() {
+    public String index() {
         return "Hello World!";
     }
     
