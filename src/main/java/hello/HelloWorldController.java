2772f448d75237b6e6811494cc514ae4ad910281
Naveen Anjuru <Naveen.Anjuru@synechron.com>
2017-01-24 17:11:00 +0530

    hanges Class Names

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class HelloWorldController {
     
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String hello() {
+        return "Hello World!";
     }
     
     @RequestMapping("/am")
